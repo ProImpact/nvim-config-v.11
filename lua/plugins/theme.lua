f1679473957b26ea3fe1b6d92c63944aea4a6456
Anthony <anthonyguera551@gmail.com>
2025-09-04 02:54:42 -0400

    Add transparent background

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,12 +1,37 @@
 return {
-	"vague2k/vague.nvim",
+	"Mofiqul/vscode.nvim",
 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
 	priority = 1000, -- make sure to load this before all the other plugins
 	config = function()
-		-- NOTE: you do not need to call setup if you don't want to.
-		require("vague").setup({
-			-- optional configuration here
+		require("vscode").setup({
+			-- Alternatively set style in setup
+			-- style = 'light'
+
+			-- Enable transparent background
+			transparent = true,
+
+			-- Enable italic comment
+			italic_comments = true,
+
+			-- Enable italic inlay type hints
+			italic_inlayhints = true,
+
+			-- Underline `@markup.link.*` variants
+			underline_links = true,
+
+			-- Disable nvim-tree background color
+			disable_nvimtree_bg = true,
+
+			-- Apply theme colors to terminal
+			terminal_colors = true,
+
+			-- Override colors (see ./lua/vscode/colors.lua)
+			color_overrides = {
+				vscLineNumber = "#FFFFFF",
+			},
+
+			-- Override highlight groups (see ./lua/vscode/theme.lua)
 		})
-		vim.cmd("colorscheme vague")
+		vim.cmd.colorscheme("vscode")
 	end,
 }
